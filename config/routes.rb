4c35aebdb1e66a7cf2ea2976c57e197937ca83d7
Rishi-atgondan <rishijeeva13@gmail.com>
2022-08-18 17:47:03 +0530

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@
 
 Rails.application.routes.draw do
   root 'suvaicious#index'
-  get '/list_kitchens' , to:'kitchens#list_kitchens'
-  get '/kitchens/add_kitchen'
-  get '/sessions/login'
-  get '/users/new'
+  get 'list_kitchens' , to:'kitchens#list_kitchens'
+  get 'kitchens/add_kitchen'
+  get 'sessions/login'
+  get 'users/new'
   get '/home', to: 'suvaicious#index'
-  get '/about_us', to: 'suvaicious#about_us'
+  get 'about_us', to: 'suvaicious#about_us'
   get '/login', to: 'sessions#login', as: 'login'
-  get '/register', to: 'users#register'
+  get 'register', to: 'users#register'
   post '/create', to: 'users#create'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: :destroy_session
-  get '/subscription', to: 'suvaicious#subscription'
+  get 'subscription', to: 'suvaicious#subscription'
   post '/subscription', to: 'suvaicious#sub_create'
-  get '/add_kitchen', to: 'kitchens#add_kitchen'
-  get '/add_kitchen_form', to: 'kitchens#add_kitchen_form'
+  get 'add_kitchen', to: 'kitchens#add_kitchen'
+  get 'add_kitchen_form', to: 'kitchens#add_kitchen_form'
 
   post '/create_kitchen', to: 'kitchens#create'
   resources :users
