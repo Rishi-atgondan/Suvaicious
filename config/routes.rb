cdda0711121b65c1798ef289eeae9e913f38b88e
Rishi-atgondan <rishijeeva13@gmail.com>
2022-08-18 18:07:43 +0530

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # root 'suvaicious#index'
   root 'suvaicious#index'
-  get '/' , to:'suvaicious#index'
   get 'list_kitchens' , to:'kitchens#list_kitchens'
   get 'kitchens/add_kitchen'
   get 'sessions/login'
